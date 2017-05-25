e1b051618c7616146153cd583becb2c41fa5dc93
Thomas Skowron <th@skowron.biz>
2017-05-25 14:42:50 +0200

    lib/spatial: line segment to carthesian

---
+++ lib/spatial/conversion.go
@@ -0,0 +1,9 @@
+package spatial
+
+// LineSegmentToCarthesian converts a line segment into a carthesian representation.
+func LineSegmentToCarthesian(p1, p2 Point) (a, b, c float64) {
+	a = p1.Y() - p2.Y()
+	b = p2.X() - p1.X()
+	c = p2.X()*p1.Y() - p1.X()*p2.Y()
+	return
+}
