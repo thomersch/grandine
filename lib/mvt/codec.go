7fbd17d16eecf5d072f802485c6c4f00a8439fd5
Thomas Skowron <th@skowron.biz>
2017-06-11 16:40:04 +0200

    lib/mvt: encoding a tile

--- lib/mvt/codec.go
+++ lib/mvt/codec.go
@@ -1,6 +1,12 @@
 package mvt
 
-import "github.com/thomersch/grandine/lib/spatial"
+import (
+	"errors"
+
+	"github.com/golang/protobuf/proto"
+	vt "github.com/thomersch/grandine/lib/mvt/vector_tile"
+	"github.com/thomersch/grandine/lib/spatial"
+)
 
 type cmd uint32
 
@@ -12,19 +18,82 @@ const (
 	extent = 4096
 )
 
+var (
+	vtPoint        = vt.Tile_POINT
+	vtLine         = vt.Tile_LINESTRING
+	vtPoly         = vt.Tile_POLYGON
+	vtLayerVersion = uint32(2)
+)
+
 func encodeCommandInt(c cmd, count uint32) uint32 {
 	return (uint32(c) & 0x7) | (count << 3)
 }
 
-func decodeCommandInt(cmdInt uint32) (cmd, uint32) {
-	return cmd(cmdInt & 0x7), cmdInt >> 3
-}
+// func decodeCommandInt(cmdInt uint32) (cmd, uint32) {
+// 	return cmd(cmdInt & 0x7), cmdInt >> 3
+// }
 
 func encodeZigZag(i int) uint32 {
 	return uint32((i << 1) ^ (i >> 31))
 }
 
-func encodeGeometry(fs []spatial.Feature, tile TileID) (commands []uint32) {
+func EncodeTile(features map[string][]spatial.Feature, tid TileID) ([]byte, error) {
+	tile, err := assembleTile(features, tid)
+	if err != nil {
+		return nil, err
+	}
+	return proto.Marshal(&tile)
+}
+
+func assembleTile(features map[string][]spatial.Feature, tid TileID) (vt.Tile, error) {
+	var tile vt.Tile
+	for layerName, layerFeats := range features {
+		layer, err := assembleLayer(layerFeats, tid)
+		if err != nil {
+			return tile, err
+		}
+		layer.Name = &layerName
+		layer.Version = &vtLayerVersion
+		tile.Layers = append(tile.Layers, &layer)
+	}
+	return tile, nil
+}
+
+func assembleLayer(features []spatial.Feature, tile TileID) (vt.Tile_Layer, error) {
+	var (
+		tl  vt.Tile_Layer
+		err error
+		ext = uint32(4096)
+	)
+
+	// TODO: tags
+
+	for _, feat := range features {
+		var tileFeat vt.Tile_Feature
+
+		tileFeat.Geometry, err = encodeGeometry([]spatial.Geom{feat.Geometry}, tile)
+		if err != nil {
+			return tl, err
+		}
+		switch feat.Geometry.Typ() {
+		case spatial.GeomTypePoint:
+			tileFeat.Type = &vtPoint
+		case spatial.GeomTypeLineString:
+			tileFeat.Type = &vtLine
+		case spatial.GeomTypePolygon:
+			tileFeat.Type = &vtPoly
+		default:
+			return tl, errors.New("unknown geometry type")
+		}
+
+		tl.Features = append(tl.Features, &tileFeat)
+		tl.Extent = &ext //TODO: configurable?
+	}
+	return tl, nil
+}
+
+// encodes one or more geometries of the same type into one (multi-)geometry
+func encodeGeometry(geoms []spatial.Geom, tile TileID) (commands []uint32, err error) {
 	var (
 		cur    [2]uint32
 		dx, dy uint32
@@ -34,15 +103,19 @@ func encodeGeometry(fs []spatial.Feature, tile TileID) (commands []uint32) {
 		xScale, yScale   = tileScalingFactor(bbox, extent)
 		xOffset, yOffset = tileOffset(bbox)
 	)
-	for _, feat := range fs {
-		switch feat.Geometry.Typ() {
+	var typ spatial.GeomType
+	for _, geom := range geoms {
+		if typ != 0 && typ != geom.Typ() {
+			return nil, errors.New("encodeGeometry only accepts uniform geoms")
+		}
+		switch geom.Typ() {
 		case spatial.GeomTypePoint:
-			pt, _ := feat.Geometry.Point()
+			pt, _ := geom.Point()
 			tX, tY := tileCoord(pt, extent, xScale, yScale, xOffset, yOffset)
 			dx = encodeZigZag(tX - int(cur[0]))
 			dy = encodeZigZag(tY - int(cur[1]))
 			commands = append(commands, encodeCommandInt(cmdMoveTo, 1), dx, dy)
 		}
 	}
-	return
+	return commands, nil
 }
--- lib/mvt/codec_test.go
+++ lib/mvt/codec_test.go
@@ -2,6 +2,7 @@ package mvt
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/thomersch/grandine/lib/spatial"
@@ -32,14 +33,41 @@ func TestEncodeGeometry(t *testing.T) {
 
 	for n, tc := range tcs {
 		t.Run(fmt.Sprintf("%v", n), func(t *testing.T) {
-			var fts []spatial.Feature
+			var geoms []spatial.Geom
 			for _, g := range tc.geom {
 				geom, err := spatial.NewGeom(g)
 				assert.Nil(t, err)
-				fts = append(fts, spatial.Feature{Geometry: geom})
+				geoms = append(geoms, geom)
 			}
-			res := encodeGeometry(fts, TileID{X: 4, Y: 4, Z: 3})
+			res, err := encodeGeometry(geoms, TileID{X: 4, Y: 4, Z: 3})
+			assert.Nil(t, err)
 			assert.Equal(t, tc.expectedResult, res)
 		})
 	}
+}
+
+func TestEncodeTile(t *testing.T) {
+	var features []spatial.Feature
+	geoms := []interface{}{
+		spatial.Point{10, 10},
+		spatial.Point{20, 10},
+		spatial.Point{20, 30},
+	}
+
+	for _, geom := range geoms {
+		g, err := spatial.NewGeom(geom)
+		assert.Nil(t, err)
+		features = append(features, spatial.Feature{Geometry: g})
+	}
+
+	layers := map[string][]spatial.Feature{
+		"main": features,
+	}
+
+	buf, err := EncodeTile(layers, TileID{X: 0, Y: 0, Z: 1})
+	assert.Nil(t, err)
+	f, err := os.Create("testtile.mvt")
+	assert.Nil(t, err)
+	defer f.Close()
+	f.Write(buf)
 }
