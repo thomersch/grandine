e24f5c9bd298f5dc537a85b851149d905038d06d
Thomas Skowron <th@skowron.biz>
2017-07-02 17:24:16 +0200

    cmd/spatialize: separated element cache

--- cmd/spatialize/spatialize.go
+++ cmd/spatialize/spatialize.go
@@ -15,11 +15,7 @@ import (
 type dataHandler struct {
 	cond condition
 
-	// dependent objects, which will be collected in the second pass
-	depNodes    []int64
-	depNodesMtx sync.Mutex
-	depWays     []int64
-	depWaysMtx  sync.Mutex
+	ec *elemCache
 
 	nodes    []gosmparse.Node
 	nodesMtx sync.Mutex
@@ -47,10 +43,8 @@ func (d *dataHandler) ReadWay(w gosmparse.Way) {
 		if len(d.cond.value) != 0 && d.cond.value != v {
 			return
 		}
-
-		d.depNodesMtx.Lock()
-		d.depNodes = append(d.depNodes, w.NodeIDs...)
-		d.depNodesMtx.Unlock()
+		d.ec.AddNodes(w.NodeIDs...)
+		d.ec.setMembers(w.ID, w.NodeIDs)
 
 		d.waysMtx.Lock()
 		d.ways = append(d.ways, w)
@@ -71,47 +65,87 @@ func (d *dataHandler) ReadRelation(r gosmparse.Relation) {
 
 		for _, memb := range r.Members {
 			switch memb.Type {
-			case gosmparse.NodeType:
-				d.depNodesMtx.Lock()
-				d.depNodes = append(d.depNodes, memb.ID)
-				d.depNodesMtx.Unlock()
 			case gosmparse.WayType:
-				d.depWaysMtx.Lock()
-				d.depWays = append(d.depWays, memb.ID)
-				d.depWaysMtx.Unlock()
-			} // TODO: check if relations of relations are necessary
+				d.ec.AddWay(memb.ID)
+			} // TODO: check if relations of nodes/relations are necessary
 		}
 	}
 }
 
-type wayNodeCollector struct {
-	wys map[int64]struct{}
-
-	depNodes    []int64
-	depNodesMtx sync.Mutex
+type elemCache struct {
+	nodes    map[int64]spatial.Point
+	nodesMtx sync.Mutex
+	ways     map[int64][]int64
+	waysMtx  sync.Mutex
 }
 
-func (wnc *wayNodeCollector) ReadNode(n gosmparse.Node) {}
-func (wnc *wayNodeCollector) ReadWay(w gosmparse.Way) {
-	if _, ok := wnc.wys[w.ID]; ok {
-		wnc.depNodesMtx.Lock()
-		wnc.depNodes = append(wnc.depNodes, w.NodeIDs...)
-		wnc.depNodesMtx.Unlock()
+func NewElemCache() *elemCache {
+	return &elemCache{
+		nodes: map[int64]spatial.Point{},
+		ways:  map[int64][]int64{},
 	}
 }
-func (wnc *wayNodeCollector) ReadRelation(r gosmparse.Relation) {}
+
+func (d *elemCache) AddNodes(nIDs ...int64) {
+	d.nodesMtx.Lock()
+	for _, nID := range nIDs {
+		d.nodes[nID] = spatial.Point{}
+	}
+	d.nodesMtx.Unlock()
+}
+
+func (d *elemCache) AddWay(wID int64) {
+	d.waysMtx.Lock()
+	d.ways[wID] = []int64{}
+	d.waysMtx.Unlock()
+}
+
+func (d *elemCache) SetCoord(nID int64, coord spatial.Point) {
+	d.nodesMtx.Lock()
+	d.nodes[nID] = coord
+	d.nodesMtx.Unlock()
+}
+
+func (d *elemCache) setMembers(wID int64, members []int64) {
+	d.waysMtx.Lock()
+	d.ways[wID] = members
+	d.waysMtx.Unlock()
+}
+
+func (d *elemCache) ReadWay(w gosmparse.Way) {
+	d.waysMtx.Lock()
+	_, ok := d.ways[w.ID]
+	d.waysMtx.Unlock()
+	if ok {
+		d.setMembers(w.ID, w.NodeIDs)
+		d.AddNodes(w.NodeIDs...)
+	}
+}
+
+func (d *elemCache) Line(wID int64) spatial.Line {
+	// check if mutex is needed
+	membs, ok := d.ways[wID]
+	if !ok {
+		log.Fatalf("missing referenced way: %v", wID)
+	}
+
+	var l spatial.Line
+	for _, memb := range membs {
+		l = append(l, d.nodes[memb])
+	}
+	return l
+}
+
+// Interface enforces this. Probably I should change the behavior.
+func (d *elemCache) ReadNode(n gosmparse.Node)         {}
+func (d *elemCache) ReadRelation(r gosmparse.Relation) {}
 
 type nodeCollector struct {
-	nds    map[int64]spatial.Point
-	ndsMtx sync.Mutex
+	ec *elemCache
 }
 
 func (d *nodeCollector) ReadNode(n gosmparse.Node) {
-	d.ndsMtx.Lock()
-	defer d.ndsMtx.Unlock()
-	if _, ok := d.nds[n.ID]; ok {
-		d.nds[n.ID] = spatial.Point{float64(n.Lon), float64(n.Lat)}
-	}
+	d.ec.SetCoord(n.ID, spatial.Point{float64(n.Lon), float64(n.Lat)})
 }
 func (d *nodeCollector) ReadWay(w gosmparse.Way)           {}
 func (d *nodeCollector) ReadRelation(r gosmparse.Relation) {}
@@ -132,7 +166,7 @@ func main() {
 	cond := condition{"building", ""}
 
 	source := flag.String("src", "osm.pbf", "")
-	outfile := flag.String("out", "osm.cudgf", "")
+	outfile := flag.String("out", "osm.cugdf", "")
 	flag.Parse()
 
 	f, err := os.Open(*source)
@@ -142,10 +176,13 @@ func main() {
 	dec := gosmparse.NewDecoder(f)
 
 	// First pass
+	ec := NewElemCache()
 	dh := dataHandler{
 		cond: cond,
+		ec:   ec,
 	}
-	log.Println("Collecting data...")
+	log.Println("Starting 3 step parsing")
+	log.Println("Reading data (1/3)...")
 	err = dec.Parse(&dh)
 	if err != nil {
 		log.Fatal(err)
@@ -156,22 +193,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	clNds := dh.depNodes
-
 	// Second pass
-	log.Println("Collecting way nodes...")
-	wayMap := map[int64]struct{}{}
-	for _, wayID := range dh.depWays {
-		wayMap[wayID] = struct{}{}
-	}
-	wnc := wayNodeCollector{
-		wys: wayMap,
-	}
-	err = dec.Parse(&wnc)
+	log.Println("Collecting nodes (2/3)...")
+	err = dec.Parse(ec)
 	if err != nil {
 		log.Fatal(err)
 	}
-	clNds = append(clNds, wnc.depNodes...)
 
 	_, err = f.Seek(0, 0)
 	if err != nil {
@@ -179,13 +206,9 @@ func main() {
 	}
 
 	// Third pass
-	log.Println("Resolving dependent objects")
-	ndmap := map[int64]spatial.Point{}
-	for _, ndID := range clNds {
-		ndmap[ndID] = spatial.Point{}
-	}
+	log.Println("Resolving dependent objects (3/3)...")
 	rc := nodeCollector{
-		nds: ndmap,
+		ec: ec,
 	}
 	err = dec.Parse(&rc)
 	if err != nil {
@@ -194,26 +217,21 @@ func main() {
 
 	var fc []spatial.Feature
 
-	log.Println("Assembling ways")
+	log.Println("Parsing completed.")
+	log.Println("Assembling ways...")
 	// TODO: auto-detect if linestring or polygon, based on tags
 	for _, wy := range dh.ways {
 		props := map[string]interface{}{}
 		for k, v := range wy.Tags {
 			props[k] = v
 		}
-
-		var ls spatial.Line
-		for _, nID := range wy.NodeIDs {
-			ls = append(ls, rc.nds[nID])
-		}
-
 		fc = append(fc, spatial.Feature{
 			Props:    props,
-			Geometry: spatial.MustNewGeom(ls),
+			Geometry: spatial.MustNewGeom(ec.Line(wy.ID)),
 		})
 	}
 
-	log.Println("Assembling relations")
+	log.Println("Assembling relations...")
 	for _, rl := range dh.rels {
 		if v, ok := rl.Tags["type"]; !ok || v != "multipolygon" {
 			continue
@@ -242,6 +260,6 @@ func main() {
 	}
 }
 
-// func wayToLine(w gosmparse.Way) spatial.Line {
-
-// }
+func wayToLine(w gosmparse.Way) spatial.Line {
+	return nil
+}
