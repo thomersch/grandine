6a47a55ad91b04126f564fcf5fc00bddc0c2efd0
Thomas Skowron <th@skowron.biz>
2017-07-02 14:42:09 +0200

    cmd/spatialize: wip relation collector

--- cmd/spatialize/spatialize.go
+++ cmd/spatialize/spatialize.go
@@ -18,6 +18,8 @@ type dataHandler struct {
 	// dependent objects, which will be collected in the second pass
 	depNodes    []int64
 	depNodesMtx sync.Mutex
+	depWays     []int64
+	depWaysMtx  sync.Mutex
 
 	nodes    []gosmparse.Node
 	nodesMtx sync.Mutex
@@ -73,25 +75,46 @@ func (d *dataHandler) ReadRelation(r gosmparse.Relation) {
 				d.depNodesMtx.Lock()
 				d.depNodes = append(d.depNodes, memb.ID)
 				d.depNodesMtx.Unlock()
-			}
+			case gosmparse.WayType:
+				d.depWaysMtx.Lock()
+				d.depWays = append(d.depWays, memb.ID)
+				d.depWaysMtx.Unlock()
+			} // TODO: check if relations of relations are necessary
 		}
 	}
 }
 
-type relCollector struct {
+type wayNodeCollector struct {
+	wys map[int64]struct{}
+
+	depNodes    []int64
+	depNodesMtx sync.Mutex
+}
+
+func (wnc *wayNodeCollector) ReadNode(n gosmparse.Node) {}
+func (wnc *wayNodeCollector) ReadWay(w gosmparse.Way) {
+	if _, ok := wnc.wys[w.ID]; ok {
+		wnc.depNodesMtx.Lock()
+		wnc.depNodes = append(wnc.depNodes, w.NodeIDs...)
+		wnc.depNodesMtx.Unlock()
+	}
+}
+func (wnc *wayNodeCollector) ReadRelation(r gosmparse.Relation) {}
+
+type nodeCollector struct {
 	nds    map[int64]spatial.Point
 	ndsMtx sync.Mutex
 }
 
-func (d *relCollector) ReadNode(n gosmparse.Node) {
+func (d *nodeCollector) ReadNode(n gosmparse.Node) {
 	d.ndsMtx.Lock()
 	defer d.ndsMtx.Unlock()
 	if _, ok := d.nds[n.ID]; ok {
 		d.nds[n.ID] = spatial.Point{float64(n.Lon), float64(n.Lat)}
 	}
 }
-func (d *relCollector) ReadWay(w gosmparse.Way)           {}
-func (d *relCollector) ReadRelation(r gosmparse.Relation) {}
+func (d *nodeCollector) ReadWay(w gosmparse.Way)           {}
+func (d *nodeCollector) ReadRelation(r gosmparse.Relation) {}
 
 // const (
 // 	typAny      = 0
@@ -106,7 +129,7 @@ type condition struct {
 }
 
 func main() {
-	cond := condition{"highway", "primary"}
+	cond := condition{"building", ""}
 
 	source := flag.String("src", "osm.pbf", "")
 	outfile := flag.String("out", "osm.cudgf", "")
@@ -117,6 +140,8 @@ func main() {
 		log.Fatal(err)
 	}
 	dec := gosmparse.NewDecoder(f)
+
+	// First pass
 	dh := dataHandler{
 		cond: cond,
 	}
@@ -131,13 +156,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	ndmap := map[int64]spatial.Point{}
-	for _, ndID := range dh.depNodes {
-		ndmap[ndID] = spatial.Point{}
+	clNds := dh.depNodes
+
+	// Second pass
+	log.Println("Collecting way nodes...")
+	wayMap := map[int64]struct{}{}
+	for _, wayID := range dh.depWays {
+		wayMap[wayID] = struct{}{}
+	}
+	wnc := wayNodeCollector{
+		wys: wayMap,
+	}
+	err = dec.Parse(&wnc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	clNds = append(clNds, wnc.depNodes...)
+
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	// Third pass
 	log.Println("Resolving dependent objects")
-	rc := relCollector{
+	ndmap := map[int64]spatial.Point{}
+	for _, ndID := range clNds {
+		ndmap[ndID] = spatial.Point{}
+	}
+	rc := nodeCollector{
 		nds: ndmap,
 	}
 	err = dec.Parse(&rc)
@@ -148,6 +195,7 @@ func main() {
 	var fc []spatial.Feature
 
 	log.Println("Assembling ways")
+	// TODO: auto-detect if linestring or polygon, based on tags
 	for _, wy := range dh.ways {
 		props := map[string]interface{}{}
 		for k, v := range wy.Tags {
@@ -165,6 +213,24 @@ func main() {
 		})
 	}
 
+	log.Println("Assembling relations")
+	for _, rl := range dh.rels {
+		if v, ok := rl.Tags["type"]; !ok || v != "multipolygon" {
+			continue
+		}
+		var poly spatial.Polygon
+
+		for _, memb := range rl.Members {
+			if memb.Role == "outer" {
+				if len(poly) != 0 {
+					// TODO: allow polygons with multiple outer rings and split them
+					break
+				}
+				poly = append(poly, spatial.Line{})
+			}
+		}
+	}
+
 	log.Println("Writing out")
 	of, err := os.Create(*outfile)
 	if err != nil {
@@ -175,3 +241,7 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+// func wayToLine(w gosmparse.Way) spatial.Line {
+
+// }
