61c4003afc7aa053138c000d886890fae98249ee
Thomas Skowron <th@skowron.biz>
2018-01-04 23:43:29 +0100

    cmd/converter: consider mapping file writing

--- cmd/converter/converter.go
+++ cmd/converter/converter.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 
 	"github.com/thomersch/grandine/lib/csv"
 	"github.com/thomersch/grandine/lib/geojson"
+	"github.com/thomersch/grandine/lib/mapping"
 	"github.com/thomersch/grandine/lib/spaten"
 	"github.com/thomersch/grandine/lib/spaten/fileformat"
 	"github.com/thomersch/grandine/lib/spatial"
@@ -43,10 +45,29 @@ var codecs = []spatial.Codec{
 }
 
 func main() {
-	var infiles filelist
+	var (
+		infiles filelist
+		conds   []mapping.Condition
+	)
 	dest := flag.String("out", "geo.spaten", "")
+	mapFilePath := flag.String("mapping", "", "Path to mapping file which will be used to transform data.")
 	flag.Var(&infiles, "in", "infile(s)")
 	flag.Parse()
+
+	if len(*mapFilePath) != 0 {
+		mf, err := os.Open(*mapFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		conds, err = mapping.ParseMapping(mf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(conds) > 0 {
+			log.Printf("input file(s) will be filtered using %v conditions", len(conds))
+		}
+	}
 
 	enc, err := guessCodec(*dest, codecs)
 	if err != nil {
@@ -56,6 +77,12 @@ func main() {
 	if !ok {
 		log.Fatalf("%v codec does not support writing", enc)
 	}
+
+	out, err := os.Create(*dest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
 
 	var fc spatial.FeatureCollection
 	for _, infileName := range infiles {
@@ -74,6 +101,7 @@ func main() {
 		}
 		defer r.Close()
 
+		var finished func() error
 		switch d := dec.(type) {
 		case spatial.ChunkedDecoder:
 			chunks, err := d.ChunkedDecode(r)
@@ -82,25 +110,63 @@ func main() {
 			}
 			for chunks.Next() {
 				chunks.Scan(&fc)
+				finished, err = write(out, &fc, encoder, conds)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			err = finished()
+			if err != nil {
+				log.Fatal(err)
 			}
 		case spatial.Decoder:
 			err = decoder.Decode(r, &fc)
 			if err != nil {
 				log.Fatalf("could not decode %v: %v", infileName, err)
 			}
+			finished, err = write(out, &fc, encoder, conds)
+			if err != nil {
+				log.Fatal(err)
+			}
+			finished()
 		}
 	}
+}
 
-	out, err := os.Create(*dest)
-	if err != nil {
-		log.Fatal(err)
+var featBuf []spatial.FeatureCollection // TODO: this is not optimal, needs better wrapping
+
+func write(w io.Writer, fs *spatial.FeatureCollection, enc spatial.Encoder, conds []mapping.Condition) (flush func() error, err error) {
+	if len(conds) > 0 {
+		var filtered []spatial.Feature
+		for _, ft := range fs.Features {
+			for _, cond := range conds {
+				if cond.Matches(ft.Props) {
+					nft := ft
+					nft.Props = cond.Map(ft.Props)
+					filtered = append(filtered, nft)
+				}
+			}
+		}
+		fs.Features = filtered
 	}
-	defer out.Close()
-	// spew.Dump(fc)
-	err = encoder.Encode(out, &fc)
-	if err != nil {
-		log.Fatalf("could not encode %v: %v", *dest, err)
+
+	if e, ok := enc.(spatial.ChunkedEncoder); ok {
+		err = e.EncodeChunk(w, fs)
+		if err != nil {
+			return func() error { return nil }, err
+		}
+		return func() error { return e.Close() }, nil
 	}
+
+	featBuf = append(featBuf, *fs)
+	return func() error {
+		var flat spatial.FeatureCollection
+		for _, ftc := range featBuf {
+			flat.Features = append(flat.Features, ftc.Features...)
+			flat.SRID = ftc.SRID
+		}
+		return enc.Encode(w, &flat)
+	}, nil
 }
 
 func guessCodec(filename string, codecs []spatial.Codec) (spatial.Codec, error) {
