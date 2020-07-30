64e01ef3d59f3d09f6968f5c57de5e33b026a1dd
Thomas Skowron <th@skowron.eu>
2020-07-30 17:03:07 +0200

    cmd/tiler/s3: check connection early

--- cmd/tiler/s3.go
+++ cmd/tiler/s3.go
@@ -5,7 +5,9 @@ package main
 import (
 	"bytes"
 	"context"
+	"fmt"
 
+	"github.com/pkg/errors"
 	"github.com/thomersch/grandine/lib/tile"
 
 	minio "github.com/minio/minio-go/v7"
@@ -27,12 +29,20 @@ func NewS3TileWriter(endpoint, bucket, accessKeyID, secretAccessKey string) (*S3
 	s3tw := &S3TileWriter{}
 	client, err := minio.New(endpoint, &minio.Options{
 		Creds: credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-		// Secure: true,
 	})
-	s3tw.client = s3Client{client, bucket}
 	if err != nil {
-		return s3tw, err
+		return nil, err
 	}
+
+	ok, err := client.BucketExists(context.Background(), bucket)
+	if err != nil {
+		return nil, err
+	}
+	if !ok {
+		return nil, fmt.Errorf("S3 bucket %s does not exist", bucket)
+	}
+
+	s3tw.client = s3Client{client, bucket}
 
 	return s3tw, nil
 }
@@ -41,5 +51,6 @@ func (s3 *S3TileWriter) WriteTile(tID tile.ID, buf []byte, ext string) error {
 	r := bytes.NewReader(buf)
 
 	_, err := s3.client.PutObject(context.Background(), s3.client.bucket, tID.String()+"."+ext, r, int64(r.Len()), minio.PutObjectOptions{})
-	return err
+
+	return errors.Wrap(err, "S3 upload failed")
 }
